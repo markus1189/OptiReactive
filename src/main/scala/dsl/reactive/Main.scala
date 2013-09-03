5047d8d13639c204eedebfc8cd116f0b747f0b3d
Markus Hauck <markus1189@gmail.com>
2013-09-03 13:31:32 +0200

    Add ReactiveDSL trait to package dsl + scala ops

--- src/main/scala/dsl/reactive/Main.scala
+++ src/main/scala/dsl/reactive/Main.scala
@@ -2,25 +2,29 @@ import dsl.reactive._
 import dsl.reactive.simplereactive._
 import scala.virtualization.lms.common._
 
-trait Program extends Reactivity {
+trait ReactiveDSL extends Reactivity with ScalaOpsPkg
+trait ReactiveDSLExp extends ReactiveDSL with ReactivityExp with ScalaOpsPkgExp
+trait ReactiveDSLGen extends ScalaGenReactivity with ScalaCodeGenPkg {
+  val IR: ReactiveDSLExp with ScalaOpsPkgExp
+}
+
+trait Program extends ReactiveDSL {
   def f(x : Rep[Unit]) = {
     val root = ReactiveVar(unit(5))
-    val s = ISignal { root.get }
+    val s = ISignal { root.get + 1 }
 
-    println(s.get)
-
-    s
+    s.get
   }
 }
 
 object Main extends App {
-  val prog = new Program with ReactivityExp  with CompileScala { self =>
-    override val codegen = new ScalaGenEffect with ScalaGenReactivity {
+  val prog = new Program with ReactiveDSLExp  with CompileScala { self =>
+    override val codegen = new ReactiveDSLGen {
       val IR: self.type = self
     }
   }
 
   val f = prog.compile(prog.f)
 
-  println(f)
+  println(f())
 }

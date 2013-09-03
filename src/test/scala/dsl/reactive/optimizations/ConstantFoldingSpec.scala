039a6a39c7c7ae9fe1287b834c027bb258205f80
Markus Hauck <markus1189@gmail.com>
2013-09-03 16:50:31 +0200

    Test transitive constant folding

--- src/test/scala/dsl/reactive/optimizations/ConstantFoldingSpec.scala
+++ src/test/scala/dsl/reactive/optimizations/ConstantFoldingSpec.scala
@@ -8,7 +8,7 @@ import dsl.reactive._
 
 class ConstantFoldingSpec extends WordSpec with Matchers {
   "Constant Folding" should {
-    "should do nothing, if any of the dependents is not constant" in {
+    "do nothing, if any of the dependents is not constant" in {
       val prog = new CanNotBeFolded with ReactiveDSLExp with CompileScala { self =>
         override val codegen = new ReactiveDSLGen {
           val IR: self.type = self
@@ -22,8 +22,22 @@ class ConstantFoldingSpec extends WordSpec with Matchers {
       result.isInstanceOf[sr.Signal[Int]] should equal(true)
     }
 
-    "should replace with a constant if all dependencies are constant" in {
+    "replace with a constant if all dependencies are constant" in {
       val prog = new ShouldBeFolded with ReactiveDSLExp with CompileScala { self =>
+        override val codegen = new ReactiveDSLGen {
+          val IR: self.type = self
+        }
+      }
+
+      val result = {
+        prog.compile(prog.f).apply( () )
+      }.asInstanceOf[sr.Behavior[Int]]
+
+      result.isInstanceOf[sr.Constant[Int]] should equal(true)
+    }
+
+    "replace with a constant, if the reactive only depends on other constants" in {
+      val prog = new TransitiveFold with ReactiveDSLExp with CompileScala { self =>
         override val codegen = new ReactiveDSLGen {
           val IR: self.type = self
         }
@@ -41,14 +55,19 @@ class ConstantFoldingSpec extends WordSpec with Matchers {
 trait CanNotBeFolded extends ReactiveDSL {
   def f(x : Rep[Unit]) = {
     val notConstant = ReactiveVar(5)
-    val signal = ISignal { notConstant.get }
-
-    signal
+    ISignal { notConstant.get }
   }
 }
 
 trait ShouldBeFolded extends ReactiveDSL {
   def f(x : Rep[Unit]) = {
     ISignal { 42 }
+  }
+}
+
+trait TransitiveFold extends ReactiveDSL {
+  def f(x : Rep[Unit]) = {
+    val s1 = ISignal { 42 }
+    ISignal { s1.get + 1}
   }
 }
